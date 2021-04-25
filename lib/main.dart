796fe4c81a198af7d1bec1ed9fe4925e40a231e7
Shinu Mathew <shinu.mathew@samsung.com>
2021-04-26 02:48:04 +0530

    Demo completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './random_word.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,11 +7,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.green),
-        home: Scaffold(
-            appBar: AppBar(
-                centerTitle: true,
-                title: Text('Makolet', textAlign: TextAlign.center)),
-            body: Center(child: Text('Shop on the go'))));
+      theme: ThemeData(primaryColor: Colors.green),
+      home: RandomWords(),
+    );
   }
 }
+
+//  return MaterialApp(
+//         theme: ThemeData(primaryColor: Colors.green),
+//         home: Scaffold(
+//             appBar: AppBar(
+//                 centerTitle: true,
+//                 title: Text('Makolet', textAlign: TextAlign.center)),
+//             body: Center(child: Text(wordPair.asPascalCase))));
